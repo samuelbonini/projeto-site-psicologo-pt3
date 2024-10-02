6562c3bc937416d07c62790a6b745fabf0b9245d
Samuel Bonini <bonini8406@gmail.com>
2024-10-02 18:27:47 -0300

    att finalizando site

--- script.js
+++ script.js
@@ -23,3 +23,36 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 500); // Inicia a animação de escrita após 500ms
 });
 
+/* FIM */
+
+/* CARROSEL INICIO */ 
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // Pagina bullet
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      },
+      
+  }
+});
