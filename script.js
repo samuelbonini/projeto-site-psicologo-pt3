ef14db4d531b76de462b862dafa6bcdb488e3ecf
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-10-16 13:16:35 -0300

    att scroll suave

--- script.js
+++ script.js
@@ -56,3 +56,35 @@ new Swiper('.card-wrapper', {
       
   }
 });
+
+function initScrollSuave(event) {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+  
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
+  
+  
+  
+  /*FORMA ALTERNATIVA
+  const topo = section.offsetTop;
+  window.scrollTo ({
+    top: topo,
+    behavior: 'smooth',
+  })
+  FORMA ALTERNATIVA*/
+  
+  }
+  
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  });
+  }
+  
+  initScrollSuave();
